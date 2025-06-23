b6c6f8e883aa61e72e1f106615d4aa126dfc95f6
yashdhimangethub48 <yashdhiman48@gmail.com>
2025-06-23 18:12:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -45,4 +45,6 @@ def download(filename):
     return send_file(os.path.join(UPLOAD_FOLDER, filename), as_attachment=True)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
